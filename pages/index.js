61e79d1afa2c8135be5dfbe8498fed552db15474
alvis <alvis@designdirect.io>
2022-02-24 11:05:02 +0000

    final commit

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,10 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome to <a href='https://nextjs.org'>Next.js!</a>
+					Welcome to my <a>Card Demo!</a>
 				</h1>
 
 				<p className={styles.description}>
-					Get started by editing{' '}
 					<code className={styles.code}>pages/index.js</code>
 				</p>
 
